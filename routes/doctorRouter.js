3ce934662d33f1ead310932e2df6c026e990e43b
sauravprajapatiii <sauravprajapati1411@gmail.com>
2026-04-28 18:34:57 +0530

    ff

--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -10,7 +10,8 @@ import {
   updateDoctor,
 } from "../controllers/doctorController.js";
 import doctorAuth from "../middlewares/doctorAuth.js";
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+import upload from "../middlewares/multer.js";
 const doctorRouter = express.Router();
 doctorRouter.get("/", getDoctors);
 doctorRouter.post("/login", doctorLogin);
@@ -18,5 +19,22 @@ doctorRouter.get("/:id", getDoctorById);
 doctorRouter.post("/", upload.single("image"), createDoctor);
 doctorRouter.post("/:id/toggle-availibility", doctorAuth, toggleAvailibility);
 doctorRouter.put("/:id", doctorAuth, upload.single("image"), updateDoctor);
+doctorRouter.put(
+  "/:id",
+  doctorAuth,
+  (req, res, next) => {
+    upload.single("image")(req, res, function (err) {
+      if (err) {
+        console.error("🔥 MULTER ERROR:", err);
+        return res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      }
+      next();
+    });
+  },
+  updateDoctor,
+);
 doctorRouter.delete("/:id", deleteDoctor);
 export default doctorRouter;
